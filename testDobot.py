793f6b4d4f56d3af8d934f2b232fc04a058620e3
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2024-12-17 19:03:00 +0200

    test file update

--- testDobot.py
+++ testDobot.py
@@ -2,7 +2,7 @@ from dobot import DobotController
 
 
 
-
+dobot = DobotController()
 device = dobot.device
 (x, y, z, r, j1, j2, j3, j4) = dobot.get_current_pos()
 device.move_to(x + 20, y, z, r, wait=False)
