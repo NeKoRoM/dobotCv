502ab34046599c558653043b29bd4254379d6160
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2024-12-27 19:34:37 +0000

    fix: enhance image rotation processing with linear interpolation and border replication

--- camTk.py
+++ camTk.py
@@ -412,8 +412,8 @@ class CameraProcessor:
                 (h, w) = self.output_image.shape[:2]
                 center = (w // 2, h // 2)
                 M = cv2.getRotationMatrix2D(center, angle, 1.0)
-                self.output_image = cv2.warpAffine(self.output_image, M, (w, h))
-                self.image = cv2.warpAffine(self.image, M, (w, h))
+                self.output_image = cv2.warpAffine(self.output_image, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
+                self.image = cv2.warpAffine(self.image, M, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
                 result += f"Contour {i}: Area={area}, Angle={angle}\n"
                 break
 
