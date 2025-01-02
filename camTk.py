560c4a8070f194c5489bc1077e674ba1124aea58
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2025-01-02 15:46:14 +0000

    fix: comment out drawing of right halves and vertical line in CameraProcessor

--- camTk.py
+++ camTk.py
@@ -423,10 +423,10 @@ class CameraProcessor:
                     half_parent_right = parent_contour[parent_contour[:, :, 0] >= cX]
                     cv2.drawContours(self.output_image, [half_contour], -1, (0, 255, 0), 1)
                     cv2.drawContours(self.output_image, [half_parent], -1, (255, 255, 0), 1)
-                    cv2.drawContours(self.output_image, [half_contour_right], -1, (0, 255, 0), 1)
-                    cv2.drawContours(self.output_image, [half_parent_right], -1, (255, 255, 0), 1)
+                    #cv2.drawContours(self.output_image, [half_contour_right], -1, (0, 255, 0), 1)
+                    #cv2.drawContours(self.output_image, [half_parent_right], -1, (255, 255, 0), 1)
                     #draw vertical line from center of main contour
-                    cv2.line(self.output_image, (cX, 0), (cX, 480), (0, 255, 255), 1)
+                    #cv2.line(self.output_image, (cX, 0), (cX, 480), (0, 255, 255), 1)
                     
 
 
