1a22e57dd03b570d620b1753158324de9b10e147
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2025-01-02 19:51:03 +0000

    fix: update preview configuration size in CameraProcessor to improve image quality

--- camTk.py
+++ camTk.py
@@ -383,7 +383,7 @@ class CameraProcessor:
     def analyze_image(self, camera_settings):
         # Initialize the camera
         self.picam2 = Picamera2()
-        self.picam2.configure(self.picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+        self.picam2.configure(self.picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1536,864)}))
         self.picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": camera_settings.focus})
         self.picam2.set_controls({"ExposureTime": int(camera_settings.exposure * 10)})  # Ensure exposure time is an integer
         self.picam2.start()
