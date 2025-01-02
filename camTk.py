62b1081c09cd0d9068be05db2ccc14238f8b7690
NeKoRoM <68150908+NeKoRoM@users.noreply.github.com>
2025-01-02 18:18:25 +0000

    fix: correct area label display in CameraProcessor output image

--- camTk.py
+++ camTk.py
@@ -467,7 +467,6 @@ class CameraProcessor:
                     cv2.drawContours(self.output_image, [half_parent], -1, (255, 0, 0), 1)
                     right_area = cv2.contourArea(half_parent_right) - cv2.contourArea(half_contour_right)
                     left_area = cv2.contourArea(half_parent) - cv2.contourArea(half_contour)
-                    cv2.putText(self.output_image, f"arreaRL: ({right_area}, - {left_area} = {right_area - left_area})", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
                     # Draw contours for the right half
 
                     # Split contours based on the center Y coordinate
@@ -485,6 +484,8 @@ class CameraProcessor:
                     bottom_area = cv2.contourArea(half_parent_bottom) - cv2.contourArea(half_contour_bottom)
                     
                     # Display the calculated areas on the image
+                    cv2.putText(self.output_image, f"arreaRL: ({right_area}, - {left_area} = {right_area - left_area})", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
                     cv2.putText(self.output_image, f"areaTB: ({top_area}, - {bottom_area} = {top_area - bottom_area})", (cX, cY+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
 
